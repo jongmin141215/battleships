45729a567bb8c2858b8250c61ca7e3bf9d899646
Proskurina <yproskurina@yahoo.com>
2015-09-09 20:16:49 +0100

    Submarine can find all positions

--- spec/submarine_spec.rb
+++ spec/submarine_spec.rb
@@ -1,4 +1,25 @@
 require 'submarine'
 
 describe Submarine do
+
+  it "knows all positions when facing north" do
+    sub = Submarine.new("B2", :N)
+    expect(sub.all_positions).to eq(['B2', 'B1'])
+  end
+
+  it "knows all positions when facing south" do
+    sub = Submarine.new("B2", :S)
+    expect(sub.all_positions).to eq(['B2', 'B3'])
+  end
+
+  it "knows all positions when facing west" do
+    sub = Submarine.new("B2", :W)
+    expect(sub.all_positions).to eq(['B2', 'A2'])
+  end
+
+  it "knows all positions when facing west" do
+    sub = Submarine.new("B2", :E)
+    expect(sub.all_positions).to eq(['B2', 'C2'])
+  end
+
 end
