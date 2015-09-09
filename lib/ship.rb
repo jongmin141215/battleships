8d9e1c741006906c7ef0437883c6066daae2115b
Jongmin Kim <kjm141215@gmail.com>
2015-09-09 12:27:00 +0100

    Figuring out double

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,11 +1,14 @@
 class Ship
+
   attr_reader :position
+
   def initialize(position, direction)
     @position = position
     @direction = direction
     @size = 1
     @hits = 0
   end
+  
 end
 
 # Version 'Skateboard'
